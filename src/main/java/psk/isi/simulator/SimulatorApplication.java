104e56716ec9dfc432c6ec1e57eac921cb7af765
Akrel <55836587+Akrel@users.noreply.github.com>
2021-04-26 20:28:46 +0200

    Add files via upload

---
+++ src/main/java/psk/isi/simulator/SimulatorApplication.java
@@ -0,0 +1,13 @@
+package psk.isi.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/psk/isi/simulator/SimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package psk.isi.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
